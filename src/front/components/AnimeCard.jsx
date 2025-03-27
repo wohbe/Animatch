983bed022f7838f11163dfd6dbb2b832d19b1b4b
David <lizarte41@gmail.com>
2025-03-27 18:44:51 +0000

    vista de prueba de anime

---
+++ src/front/components/AnimeCard.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import '../index.css';
+
+function AnimeCard() {
+  const anime = {
+    title: 'Chainsaw Man',
+    synopsis:
+      'Denji es un adolescente que vive con un demonio motosierra llamado Pochita. Para pagar la deuda que le dejó su padre tras su muerte, ha tenido que ganarse el pan como puede matando demonios y vendiendo sus cadáverse a la mafia, aunque su vida siempre ha sido miserable.',
+    coverImage:
+      'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx127230-NuHM32a3VJsb.png',
+    genres: ['Acción', 'Aventura', 'Fantasía'],
+    status: 'En emisión',
+    score: 8.5,
+    episodes: 12,
+    seasons: 1,
+    startDate: '2023-10-26',
+    endDate: 'En emisión',
+    studio: 'Estudio',
+    director: 'Director',
+  };
+
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isWatchLater, setIsWatchLater] = useState(false);
+  const [isWatched, setIsWatched] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setIsFavorite(!isFavorite);
+    if (isFavorite) {
+      console.log('Eliminado de favoritos:', anime.title);
+    } else {
+      console.log('Añadido a favoritos:', anime.title);
+    }
+  };
+
+  const handleWatchLaterClick = () => {
+    setIsWatchLater(!isWatchLater);
+    if (isWatchLater) {
+      console.log('Eliminado de ver más tarde:', anime.title);
+    } else {
+      console.log('Añadido a ver más tarde:', anime.title);
+    }
+  };
+
+  const handleWatchedClick = () => {
+    setIsWatched(!isWatched);
+    if (isWatched) {
+      console.log('Marcado como no visto:', anime.title);
+    } else {
+      console.log('Marcado como visto:', anime.title);
+    }
+  };
+
+  return (
+    <div className='container'>
+      <div className="anime-card d-flex m-2">
+        <img src={anime.coverImage} alt={anime.title} className="cover-image" />
+        <div className='card-body px-3'>
+          <h1>{anime.title}</h1>
+          <p>{anime.synopsis}</p>
+          <div className="genres">Genre:&nbsp;
+    {anime.genres.map((genre, index) => (
+        <span key={genre} className="genre">
+            {genre}{index < anime.genres.length - 1 ? ', ' : ''}
+        </span>
+    ))}
+</div>
+          <p>Estado: {anime.status}</p>
+          <p>Puntuación: {anime.score}</p>
+          <p>Episodios: {anime.episodes}</p>
+          <p>Temporadas: {anime.seasons}</p>
+          <p>Fecha de inicio: {anime.startDate}</p>
+          <p>Fecha de fin: {anime.endDate}</p>
+          <p>Estudio: {anime.studio}</p>
+          <p>Director: {anime.director}</p>
+
+          <div className="anime-buttons d-flex justify-content-around">
+            <button
+              className="favorite-button btn btn-primary"
+              onClick={handleFavoriteClick}
+            >
+              {isFavorite ? 'Quitar de favoritos' : 'Favoritos'}
+            </button>
+            <button
+              className="watch-later-button btn btn-primary"
+              onClick={handleWatchLaterClick}
+            >
+              {isWatchLater ? 'Quitar de ver más tarde' : 'Ver más tarde'}
+            </button>
+            <label className="watched-button btn btn-primary">
+            <input
+              type="checkbox"
+              checked={isWatched}
+              onChange={handleWatchedClick}
+             />Visto
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AnimeCard;
--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,40 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
-
+const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<section>
+			<div className="NavBar">
+				<nav className="navbar navbar-expand-lg">
+					<a class="navbar-brand" href="#">
+						<img src="/src/front/assets/img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="" />
+					</a>
+					<div className="container-fluid">
+						<a className="navbar-brand Home" href="/">Animatch</a>
+						<button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+							data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div className="navbar-nav">
+								<Link className="nav-link active News" aria-current="page" href="#">New</Link>
+								<Link className="nav-link active Popular" aria-current="page" href="#">Popular</Link>
+								<Link className="nav-link active Categories" aria-current="page" href="#">Categories</Link>
+								<Link className="nav-link active Match" href="#">Match <i className="fa-regular fa-heart"></i></Link>
+								<Link className="nav-link active Profile" href="#">Profile</Link>
+							</div>
+						</div>
+					</div>
+					<form class="form-inline  my-lg-0 d-flex">
+						<input class="form-control mr-sm-3 "type="search" placeholder="Search" aria-label="Search" />
+						<button class="btn btn-outline-success  my-sm-0 m-2" type="submit">Search</button>
+					</form>
+				</nav>
 			</div>
-		</nav>
+		</section>
+
 	);
 };
+
+export default Navbar;
