ff7fbe33823c48bcad3cb6c2c49c6d559cfbbeab
Rober <robercantalejo@gmail.com>
2025-04-03 18:16:59 +0000

    [ADD] Import JWTManager and configured JWT secret key

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-
+from flask_jwt_extended import JWTManager
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -18,6 +18,10 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "98a7b6c5d4e3f2g1h0i9j8k7l6m5n4o3p2q1r0s9t8u7v6w5x4y3z2a1b0c9d8e7f6"
+jwt = JWTManager(app)
 
 # Configuración de SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
