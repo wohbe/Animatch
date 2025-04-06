aa781d1d5228c1a8c9f28758a5cecbb59bfefb6b
Domenico Puzone <137282500+domeseo@users.noreply.github.com>
2025-04-06 17:51:16 +0000

    Creation Animatch page and routes jsx

---
+++ src/front/pages/AnimatchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Animatch from "../components/Animatch";
+
+export const AniPage = () => {
+
+    return (
+        <>
+            <Animatch />
+        </>
+    )
+}
